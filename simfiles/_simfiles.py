1cf89723b155945c32721a59e69f23dcea881fae
K.A. Oman <koman@astro.rug.nl>
2019-03-12 12:30:10 +0100

    Added docstrings.

--- simfiles/_simfiles.py
+++ simfiles/_simfiles.py
@@ -9,6 +9,55 @@ from os.path import expanduser
 
 
 class SimFiles(dict):
+    """
+    Provides a generic interface to simulation hdf5 files.
+
+    SimFiles is a dict with added features, notably __getattr__ and
+    __setattr__, and automatic loading of data from simulation files based on a
+    configuration file.
+
+    Parameters
+    ----------
+    snap_id : index
+        An identifier for a specific simulation snapshot. The exact format is
+        defined in the configuration for the simulation in question.
+
+    configfile : str
+        Path to the configuration file to use (default: None).
+
+    ncpu : int
+        Number of processors on which to run (default: 2).
+
+    share_mode : bool
+        Setting 'True' disables the __delitem__ method (default: False) and
+        suppresses warnings for repeated loading of the same keys.
+
+    single_file : int
+        Specify to load from only a specific hdf5 file 'piece' of the snapshot.
+        Assumes 'pieces' end in '.X.hdf5' where X is an integer.
+
+    Returns
+    -------
+    out : SimFiles
+        A SimFiles object configured using the file provided.
+
+    Examples
+    --------
+    The following example sets up a SimFiles instance, loads a few keys, and
+    accesses the loaded data, for APOSTLE simulation data on the cavi system::
+
+        from simfiles import SimFiles
+        from simfiles.configs.APOSTLE_cavi import __file__ as configfile
+        import namedtuple
+        snap_id = namedtuple('snap_id', ['res', 'phys', 'vol', 'snap'])
+        mysnap = snap_id(res=3, phys='hydro', vol=1, snap=127)
+        SF = SimFiles(mysnap, configfile=configfile)
+        SF.load(keys=('m_s', 'xyz_s', 'vxyz_s'))
+        # print mass and coordinates of one particle
+        # both dict-like and attribute-like access are supported
+        # this config file supports units via astropy.units
+        print(SF.m_s[0], SF['xyz_s'][0])
+    """
 
     def __init__(self, snap_id, configfile=None, ncpu=2, share_mode=False,
                  single_file=None):
@@ -68,14 +117,36 @@ class SimFiles(dict):
         else:
             return
 
-    def load(self, keys=None, filetype=None, intervals=None, verbose=False):
+    def load(self, keys=tuple(), filetype=None, intervals=None, verbose=False):
+        """
+        Load data for a set of keys.
 
+        Parameters
+        ----------
+        keys : iterable
+            List of keys to load (default: tuple()).
+
+        filetype : str
+            Advanced use only, override filetype defined in config file
+            (default: None).
+
+        intervals : iterable
+            List containing lists of 2-tuples, one for each key. Each 2-tuple
+            represents an interval of indices from the underlying data table
+            to load (default: None).
+
+        verbose : bool
+            Setting 'True' prints messages upon loading each key (default:
+            False).
+        """
         loaded_keys = set()
 
-        if type(keys) is not tuple:
-            raise ValueError('SimFiles.load: keys must be tuple.')
+        try:
+            keys = tuple(keys)
+        except TypeError:
+            raise TypeError("SimFiles.load: keys must interpretable as tuple.")
 
-        if (keys is not None) and (intervals is None):
+        if (keys != tuple()) and (intervals is None):
             intervals = (None, ) * len(keys)
         for key, interval in zip(keys, intervals):
             loaded_keys.update(self._load_key(
@@ -88,6 +159,14 @@ class SimFiles(dict):
         return loaded_keys
 
     def fields(self, keytype='all'):
+        """
+        Return a list of available keys, optionally for a specific keytype.
+
+        Parameters
+        ----------
+        keytype : str
+            Specify which type of keys to include (default: 'all').
+        """
         if keytype == 'all':
             return [k for k in self._extractors.keys()]
         else:
